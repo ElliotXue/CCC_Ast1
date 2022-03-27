c69f560bfd7aaa01942a1da55cd940972b7129f5
ElliotXue <61861224+ElliotXue@users.noreply.github.com>
2022-03-27 17:22:24 +0800

    language.json

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ with open("tinyTwitter.json", "r+b") as f:
     for line in iter(mm.readline, b""):
         str_line = line.decode().strip()
         lst.append(str_line)
-    head_row = lst[0]+lst[-1]
+    head_row = lst[0] + lst[-1]
     rest_lst = lst[1:-1]
     for row in rest_lst:
         new_row = row[:-1]
         obj = json.loads(new_row)
 
-
-
-
-
+f1 = open("language.json")
+language = json.load(f1)
+print(language)
